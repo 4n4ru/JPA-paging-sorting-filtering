70042101fee56ab19d3ec1733b33287386af0309
Ana Runje <ana.runje@clover.com>
2023-09-26 12:18:42 +0100

    Create Entity

---
+++ src/main/java/dev/runje/ana/jpapagingsortingfiltering/model/Employee.java
@@ -0,0 +1,46 @@
+package dev.runje.ana.jpapagingsortingfiltering.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EMPLOYEE")
+public class Employee {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String firstName;
+  private String lastName;
+
+  public Employee() {
+  }
+
+  public Employee(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
